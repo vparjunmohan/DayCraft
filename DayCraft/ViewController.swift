081ba1792aa75cce93b0c2854aa86c04184d8747
Arjun Mohan <vparjunmohan@gmail.com>
2024-02-05 16:33:51 +0700

    initial setup

---
+++ DayCraft/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  DayCraft
+//
+//  Created by Arjun Mohan on 05/02/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
